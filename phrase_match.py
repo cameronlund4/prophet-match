3fbfdf2bad9850dad8d9c61bacb844e11e43ecb8
Cameron Lund <cameron@discostudios.io>
2022-04-18 23:49:42 -0400

    Clarification

--- phrase_match.py
+++ phrase_match.py
@@ -1,11 +1,19 @@
 def is_phrase_match(match: str, body: str) -> bool:
     """
-    Check if "match" is in "body".
+    We are searching some profiles on a social media site for some
+    keywords.
 
-    This is to solve an issue we are having with checking for keywords in
-    peoples profiles. For example, right now, searching for "COO" will
-    return true against "Coordinator". We do not want this to be true.
+    We want to check if a given phrase, "match", is in a given
+    body of text, "body".
 
-    We do, however, want to match "COO" against "I am the coo".
+    Currently, our solution is broken. If we are searching for "coo",
+    we will get a true return for "coordinator". We only want to look
+    for people with "coo" on it's own, such as "I am the COO".
+
+    We want to match case insensitively. So, "coo" and "COO" should be True.
+
+    Once that is working, try and get it to work ignoring non-alpha characters.
+    For example, return true with match of "coo" and body of
+    "I am the COO: chief operating officer!"
     """
-    return False
+    return match.lower() in body.lower()
